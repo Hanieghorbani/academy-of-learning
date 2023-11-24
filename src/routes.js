90d0a13e0ee31eec982c99ebaf40f94d5c2f6fe4
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-11-24 14:13:54 +0330

    create sidebar&topbar Admin panel

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,11 @@ import Register from "./pages/Register/Register"
 import Articles from "./pages/Articles/Articles"
 import Contact from "./pages/Contact/Contact"
 import Search from "./pages/search/Search"
+import AdminPanel from './pages/AdminPanel/index'
+import Users from "./pages/AdminPanel/Users/Users"
+import AdminCourses from './pages/AdminPanel/Courses/Courses'
+import Menus from "./pages/AdminPanel/Menus/Menus"
+import AdminArticles from './pages/AdminPanel/Articles/Articles'
 const routes = [
     { path: '/', element: <Index /> },
     { path: '/course-info/:courseName', element: <CourseInfo /> },
@@ -19,6 +24,12 @@ const routes = [
     { path: '/register', element: <Register /> },
     { path: '/contact', element: <Contact /> },
     { path: '/search/:searchValue', element: <Search /> },
+    { path: '/p-admin/*', element: <AdminPanel /> ,children:[
+        { path: 'users', element: <Users /> }, 
+        { path: 'courses', element: <AdminCourses/> }, 
+        { path: 'menus', element: <Menus/> }, 
+        { path: 'articles', element: <AdminArticles/> }, 
+    ]},
 
 ]
 
