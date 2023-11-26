91f31932505f19f0370702dfb41674264d70b223
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-11-26 14:39:46 +0330

    create category page for p-admin

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ import Users from "./pages/AdminPanel/Users/Users"
 import AdminCourses from './pages/AdminPanel/Courses/Courses'
 import Menus from "./pages/AdminPanel/Menus/Menus"
 import AdminArticles from './pages/AdminPanel/Articles/Articles'
+import AdminCategory from './pages/AdminPanel/Category/Category'
 const routes = [
     { path: '/', element: <Index /> },
     { path: '/course-info/:courseName', element: <CourseInfo /> },
@@ -29,6 +30,7 @@ const routes = [
         { path: 'courses', element: <AdminCourses/> }, 
         { path: 'menus', element: <Menus/> }, 
         { path: 'articles', element: <AdminArticles/> }, 
+        { path: 'category', element: <AdminCategory/> }, 
     ]},
 
 ]
