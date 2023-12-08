4630b9f3b8ae174941196c429aae25ba28536dcd
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-12-08 16:57:52 +0330

    remove some packages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
-import "react-toastify/dist/ReactToastify.css"
 import { BrowserRouter } from 'react-router-dom';
 import './styles/reset.css'
 import './styles/fonts.css'
